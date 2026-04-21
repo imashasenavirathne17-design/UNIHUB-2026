16fa408a51ffce9fd9eb61e598e9247deed26bba
IT23624030 <it23624030@my.sliit.lk>
2026-04-21 15:37:45 +0530

    add backend controllers file

--- backend/routes/labhallsystem_AuthRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login } = require('../controllers/labhallsystem_AuthController');
-
-router.post('/login', login);
-
-module.exports = router;
